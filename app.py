c9f316de3a697113cd4923758675a6715a353182
Andrew Gentry <agentdevr@gmail.com>
2016-04-20 09:02:48 -0600

    s

--- app.py
+++ app.py
@@ -112,7 +112,7 @@ def names():
 			email.append(record[2])
 			distributor.append(record[3])
 			salesperson.append(record[4])	
-		return '<br>'.join(salesperson) + ' - '.join(distributor) + ' - '.join(email) + ' - '.join(psswrd) + ' - '.join(name)
+		return name
 		 
 
 	except psycopg2.DatabaseError as e:
